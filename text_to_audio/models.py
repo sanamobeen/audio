dd96c80091eb623c6d63b0b66ecc2486455e8b49
Sana Mobin <sanamobin7@gmail.com>
2025-06-16 18:01:51 +0500

    final changes in text_to_audio app

--- text_to_audio/models.py
+++ text_to_audio/models.py
@@ -6,6 +6,7 @@ class Audio(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
     audio_file = models.FileField(upload_to="generated/")
+    language = models.CharField(max_length=10, default='en')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     converted_text = models.TextField(blank=True, null=True)
 
--- text_to_audio/urls.py
+++ text_to_audio/urls.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.urls import path
 from accounts import views
-from .views import TextToSpeechView
+from .views import TextToSpeechView,ListOfAudios,SelectLanguageOfAudio
 
 
 urlpatterns = [
     path("text_to_speech/", TextToSpeechView.as_view(), name="Text_To_Speech"),
+    path("select_language_of_audio/", SelectLanguageOfAudio.as_view(), name="select_language_of_audio"),
+    path("list_of_audios/", ListOfAudios.as_view(), name="List_Of_Audios"),
+
 ]
